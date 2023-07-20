350f97d6dcd83458b938c84fb8ea5922b385ac44
Peter Antony Rausch <peter.rausch@premiersoft.net>
2023-07-19 21:30:07 -0300

    Programando entidade Endereço;

--- src/main/java/mediconnect/modelo/entidade/endereco/Endereco.java
+++ src/main/java/mediconnect/modelo/entidade/endereco/Endereco.java
@@ -1,5 +1,25 @@
 package main.java.mediconnect.modelo.entidade.endereco;
 
 public class Endereco {
+    private String cep;
+    private String logradouro;
+    private String numero;
 
+    public Endereco(String cep, String logradouro, String numero) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro(){
+        return logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
 }
