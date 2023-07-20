1d870e0cf79cdb5a75a12286613ba1c170d9e76e
Peter Antony Rausch <peter.rausch@premiersoft.net>
2023-07-19 21:35:24 -0300

    mudei umas paradas ai

--- src/main/java/mediconnect/modelo/entidade/endereco/Endereco.java
+++ src/main/java/mediconnect/modelo/entidade/endereco/Endereco.java
@@ -1,32 +1,14 @@
 package main.java.mediconnect.modelo.entidade.endereco;
 
 public class Endereco {
-    private String cep;
-    private String logradouro;
-    private String numero;
     private String bairro;
     private String cidade;
     private String estado;
 
-    public Endereco(String cep, String logradouro, String numero, String bairro, String cidade, String estado) {
-        this.cep = cep;
-        this.logradouro = logradouro;
-        this.numero = numero;
+    public Endereco(String bairro, String cidade, String estado) {
         this.bairro = bairro;
         this.cidade = cidade;
         this.estado = estado;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public String getLogradouro(){
-        return logradouro;
-    }
-
-    public String getNumero() {
-        return numero;
     }
 
     public String getBairro() {
